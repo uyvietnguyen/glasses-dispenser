7dc17c5feef24d8f660462e140b51eba4fa93783
Uyviet Nguyen <uyvietnguyen@Jennifers-MacBook-Pro.local>
2019-10-04 12:43:38 +0900

    trying to run

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
-from flask import Flask
-# from . import models, routes, services
-from . import routes
-app = Flask(__name__)
-
-def create_app():
-    # models.init_app(app)
-    routes.init_app(app)
-    # services.init_app(app)
-    return app
+from flask import current_app
 
 if __name__ == '__main__':
-    app.run()
+    current_app.run()
--- routes/page.py
+++ routes/page.py
@@ -2,5 +2,5 @@ from flask import current_app
 
 @current_app.route("/")
 def hello():
-    return "Hello World!"
+    return "YEET ICE YEET!"
 
